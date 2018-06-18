5a75136b73d1b772a679c6938b0545ee195bf807
Keith Hill <khill@acquisio.com>
2018-06-18 13:03:12 -0400

    cleanup; added environments;

--- elements/package-chooser/gulp-tasks/demoing.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-const ghPages  = require('gulp-gh-pages');
-
-gulp.task('pages', function() {
-  return gulp.src(["./index.html","./demo/*","./test/*", "./bower_components/**/*"],{base: '.'})
-  .pipe(ghPages());
-});
